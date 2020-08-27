fd8618799b62b4dd104f9ad77b7750c8b794d5e1
Rodney Smith II <r_smith3@hotmail.com>
2020-08-27 04:33:06 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,7 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  assembled_matrix = [ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE]
+  assembled_matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
@@ -26,15 +26,15 @@ def assembled_matrix
 end
 
 def sorted_matrix
-  assembled_matrix = [ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE]
-  sorted_matrix = [ORGANIC_PRODUCE.sort, CONVENTIONAL_PRODUCE.sort]
+  assembled_matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+  sorted_matrix = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
 end
 
 def matrix_lookup(matrix, row, column)
-  assembled_matrix[1][1]
+  assembled_matrix[0][1]
   
     # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
